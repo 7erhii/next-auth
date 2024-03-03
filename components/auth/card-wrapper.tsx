0304636e1fbf97b7465fc6eea2e830232c151dc7
7erhiy <7erhiy@gmail.com>
2024-03-03 17:34:33 -0400

    adding inputs

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Header } from "@/components/auth/header";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { Social } from "./social";
+import { BackButton } from "./back-button";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className=" w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel}></Header>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social></Social>
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel}></BackButton>
+      </CardFooter>
+    </Card>
+  );
+};
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,81 @@
+"use client";
+import * as z from "zod";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "@/schemas";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { CardWrapper } from "./card-wrapper";
 export const LoginForm = () => {
-    return (
-        <div>
-            Login Form
-        </div>
-    )
-}
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  };
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-6">
+          <div className=" space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="john.doe@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage></FormMessage>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <FormMessage></FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button className="w-full" type="submit">
+            Login
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
